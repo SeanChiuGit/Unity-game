701e986269eb8961ad70038dd93c32563fee2fdd
swedenfish <2560275159@qq.com>
2025-02-17 00:27:22 +0000

    Revert "Merge branch 'Pick-things'"
    This reverts commit daf75e49d48ff4fb4fd73e1b402a115cb6092d56, reversing
    changes made to 2b4532cb061f4b52422baca111d836dff96ab56e.

--- My project (2)/Assets/Scripts/PlayerSpawn.cs
+++ My project (2)/Assets/Scripts/PlayerSpawn.cs
@@ -1,3 +1,54 @@
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+
+// public class PlayerSpawn : MonoBehaviour
+// {
+//     void Awake()
+//     {
+//         DontDestroyOnLoad(gameObject); // 确保 Player 在场景切换时不会被销毁
+//     }
+
+//     void OnEnable()
+//     {
+//         SceneManager.sceneLoaded += OnSceneLoaded; // 监听场景加载
+//     }
+
+//     void OnDisable()
+//     {
+//         SceneManager.sceneLoaded -= OnSceneLoaded; // 取消监听，防止重复调用
+//     }
+//     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+//     {
+//         GameObject spawnPoint = GameObject.FindGameObjectWithTag("Respawn"); // 确保 SpawnPoint 的 Tag 设为 "Respawn"
+//         Debug.Log("a");
+
+//         if (spawnPoint != null)
+//         {
+
+//             PlayerController controller = GetComponent<PlayerController>();
+//             if (controller != null)
+//             {
+//                 controller.enabled = false; // 禁用 PlayerController，防止位置被覆盖
+//             }
+//             StartCoroutine(EnableControllerAfterDelay(controller));
+
+
+//             // transform.position = spawnPoint.transform.position; // 传送到该点
+//             Vector3 worldPosition = spawnPoint.transform.TransformPoint(Vector3.zero);
+//             Debug.Log(worldPosition);
+//             transform.position = worldPosition; // 传送到世界坐标
+
+//             IEnumerator EnableControllerAfterDelay(PlayerController controller)
+//             {
+//                 yield return null; // 等待一帧
+//                 controller.enabled = true; // 重新启用 PlayerController
+//             }
+//         }
+//     }
+// }
+
+
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
